47bde0a5e9a53ae3fe96b9755b0db5bb6abf55e0
Subasri <75534391+balasarathi@users.noreply.github.com>
2021-01-27 19:10:33 +0530

    Update and rename help.js to index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+<head>
+
+<h3>html bot</h3>
+<p>hi my name is html bot IAM an HTML bot to review HTML 
+Group manager bot by balasarathi and subasri
+
+<button TYPE ="button" onclick=document.getELEmentBYId('help')
+help</button>
+
+<p id="help">  
+
+Hey! My name is HTML bot. I am a group management bot, here to help you get around and keep the order in your groups!
+I have lots of handy features, such as flood control, a warning system, a note keeping system, and even predetermined replies on certain keywords.
+
+Helpful commands:
+- /start: Starts me! You've probably already used this.
+- /help: Sends this message; I'll tell you more about myself!
+- /donate: Gives you info on how to support me and my creator.
+
+If you have any bugs or questions on how to use me, have a look at my website, or head to @vickyorflowerbotnews.
+ All commands can be used with the following: / !</p>
